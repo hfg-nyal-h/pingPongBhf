241a08b19af87906c27f85a489768802785ece0c
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-05-30 20:57:57 +0200

    change the controll A D and Arrow left right

--- p5_pong/js/sketch.js
+++ p5_pong/js/sketch.js
@@ -8,7 +8,6 @@ function preload() {
 function setup() {
     createCanvas(400, 700)
     ball = new Ball(width / 2, height / 2, 10, 10);
-    //TODO akteull ist das Paddle p2 noch nicht richtig im bild reim Rendern 
     p1 = new Paddle(width / 2 - 50, 20, 100, 10);
     p2 = new Paddle(width / 2 - 50, height - 50, 100, 10);
 }
@@ -42,23 +41,24 @@ function draw() {
 
 
 function movePaddles() {
+	// asci II code 
     // 65 = 'a'
     if (keyIsDown(65)) {
         p1.move(-5);
     }
 
-    // 90 = 'z'
-    if (keyIsDown(90)) {
+    // 68 = 'd'
+    if (keyIsDown(68)) {
         p1.move(5);
     }
 
-    // 75 = 'k'
-    if (keyIsDown(75)) {
+    
+    if (keyIsDown(LEFT_ARROW)) {
         p2.move(-5);
     }
 
-    // 77 = 'm'
-    if (keyIsDown(77)) {
+    
+    if (keyIsDown(RIGHT_ARROW)) {
         p2.move(5);
     }
 }
