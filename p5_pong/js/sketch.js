bbef4eff15961de321bc4acb7aa8ce4adea3b97f
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2022-05-30 22:56:02 +0200

    add the posenet controler

--- p5_pong/js/sketch.js
+++ p5_pong/js/sketch.js
@@ -1,6 +1,11 @@
 let ball, p1, p2, retroFont;
 let go = false;
 
+let video;
+let poseNet;
+let poses = [];
+let avarage;
+let lastavarage ;
 function preload() {
     retroFont = loadFont('ARCADECLASSIC.TTF');
 }
@@ -10,14 +15,16 @@ function setup() {
     ball = new Ball(width / 2, height / 2, 10, 10);
     p1 = new Paddle(width / 2 - 50, 20, 100, 10);
     p2 = new Paddle(width / 2 - 50, height - 50, 100, 10);
+	posenet5()
+
 }
 
 
 function draw() {
     background(52);
     backdrop();
-
     movePaddles();
+	
     p1.show();
     p2.show();
 
@@ -37,6 +44,7 @@ function draw() {
     ball.hit(p1, p2);
 
     ball.show()
+	
 }
 
 
@@ -46,21 +54,22 @@ function movePaddles() {
     if (keyIsDown(65)) {
         p1.move(-5);
     }
-
     // 68 = 'd'
     if (keyIsDown(68)) {
         p1.move(5);
     }
 
     
-    if (keyIsDown(LEFT_ARROW)) {
+    if (avarage > 250) {
         p2.move(-5);
     }
 
     
-    if (keyIsDown(RIGHT_ARROW)) {
+    if (avarage < 250) {
         p2.move(5);
     }
+	console.log(avarage)
+
 }
 
 function keyTyped() {
@@ -78,3 +87,40 @@ function keyTyped() {
     // for safety
     return false;
 }
+
+// Copyright (c) 2018 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+PoseNet example using p5.js
+=== */
+
+
+
+
+
+
+function posenet5(){
+	video = createCapture(VIDEO);
+    
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
+    poses = results;
+	avarage = (poses[0].pose.keypoints[5].position.x+poses[0].pose.keypoints[6].position.x)/2
+	lastavarage = avarage
+    //  console.log(avarage +"ich bin der Durhschnitt")
+	// console.log(lastavarage)
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+function modelReady() {
+	select('#status').html('Model Loaded');
+  }
