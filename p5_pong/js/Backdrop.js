239f5df896795e59118a10a1901a0cac6d959a59
joschroth <joschua.rothenbacher@hfg.design>
2022-05-30 20:31:19 +0200

    fixed backdrop

--- p5_pong/js/Backdrop.js
+++ p5_pong/js/Backdrop.js
@@ -7,11 +7,11 @@ function backdrop() {
 
     let dottedLength = 20;
 
-    let y = dottedLength/2;
-    
-    while (y < height) {
-        line(width/2, y, width/2, y + dottedLength);
-        y += dottedLength*2;
+    let x = dottedLength / 2;
+
+    while (x < width) {
+        line(x, height / 2, x + dottedLength, height / 2);
+        x += dottedLength * 2;
     }
 
     textFont(retroFont);
@@ -20,9 +20,9 @@ function backdrop() {
     fill(80);
 
     textAlign(RIGHT, TOP);
-    text(p1.score, width/2 - textOffsetX, textOffsetY);
+    text(p1.score, width / 2 - textOffsetX, textOffsetY);
 
     textAlign(LEFT);
-    text(p2.score, width/2 + textOffsetX, textOffsetY);
+    text(p2.score, width / 2 + textOffsetX, textOffsetY);
 
 }
