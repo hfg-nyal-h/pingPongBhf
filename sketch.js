3956d02f2c9cdd0435c775900b4e2a7cc100f1ca
hfg-nyal-h <nyal.hettmer@hfg.design>
2022-06-03 15:54:26 +0200

    zwischenstand mqtt

--- sketch.js
+++ sketch.js
@@ -16,26 +16,29 @@ let poses = [];
 let pose = [];
 let maximumPeople = 1; // max number of people to track
 
-const topic = "/pingPongBahnhof/"
-const mqttClient = mqtt.connect("http://193.196.159.141");
+// MQTT client details:
+let broker = {
+  hostname: 'mqtt.ava.hfg.design',
+  port: 443,
+};
+// MQTT client:
+let client;
+// client credentials:
+let creds = {
+  clientID: 'p5Client',
+  userName: 'public',
+  password: 'public'
+}
+// topic to subscribe to when you connect:
+let topic = 'notes';
 
+// a pushbutton to send messages
+let sendButton;
+let localDiv;
+let remoteDiv;
 
-mqttClient.on("connect", function () {
-    mqttClient.subscribe(topic, function (err) {
-        if (err) {
-            throw (err);
-        }
-        else {
-            mqttClient.publish(topic + "logs/",  + " connected " )
-        }
-    })
-    console.log("connectet")
-})
-mqttClient.on("message", async (topic, message) => {
-    console.log(message)
-  
-});
-
+// intensity of the circle in the middle
+let intensity = 255;
 
 function setup() {
   createCanvas(640, 480);
@@ -51,6 +54,34 @@ function setup() {
   });
   // Hide the video element, and just show the canvas
   video.hide();
+
+  ///////
+
+  createCanvas(400, 400);
+  // Create an MQTT client:
+  client = new Paho.MQTT.Client(broker.hostname, Number(broker.port), creds.clientID);
+  // set callback handlers for the client:
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+  // connect to the MQTT broker:
+  client.connect(
+      {
+          onSuccess: onConnect,       // callback function for when you connect
+          useSSL: true                // use SSL
+      }
+  );
+  // create the send button:
+  sendButton = createButton('send a message');
+  sendButton.position(20, 20);
+  sendButton.mousePressed(sendMqttMessage);
+  // create a div for local messages:
+  localDiv = createDiv('local messages will go here');
+  localDiv.position(20, 50);
+  localDiv.style('color', '#fff');
+  // create a div for the response:
+  remoteDiv = createDiv('waiting for messages');
+  remoteDiv.position(20, 80);
+  remoteDiv.style('color', '#fff');
 }
 
 function modelReady() {
@@ -64,6 +95,15 @@ function draw() {
   drawKeypoints();
   drawSkeleton();
   gotPoses();
+
+  background(50);
+  // draw a circle whose brightness changes when a message is received:
+  fill(intensity);
+  circle(width/2, height/2, width/2);
+// subtract one from the brightness of the circle:
+  if (intensity > 0) {
+      intensity--;
+  }
 }
 
 // A function to draw ellipses over the detected keypoints
@@ -107,16 +147,63 @@ function gotPoses(maximumPeople) {
 //if (poses.length == maximumPeople ) {
 if(poses.length > 0 ) {
   pose = poses[0].pose;
-  shoulderPose = [];
+  shoulderPose = {};
   shoulderPose.shoulderLeft = pose.keypoints[5].position.x;
   shoulderPose.confidenceLeft = pose.keypoints[5].score;
   shoulderPose.shoulderRight = pose.keypoints[6].position.x;
   shoulderPose.confidenceRight = pose.keypoints[6].score;
 
-  console.log(shoulderPose);
+  //console.log(shoulderPose);
+
+ sendMqttMessage(shoulderPose);
   
 } else if (poses.length > maximumPeople) {
   console.log("to many People")
 }
 } 
 
+
+//////
+
+
+// called when the client connects
+function onConnect() {
+  localDiv.html('client is connected');
+  client.subscribe(topic);
+}
+
+// called when the client loses its connection
+function onConnectionLost(response) {
+  if (response.errorCode !== 0) {
+      localDiv.html('onConnectionLost:' + response.errorMessage);
+  }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  remoteDiv.html('I got a message:' + message.payloadString);
+  let  incomingNumber = parseInt(message.payloadString);
+  if (incomingNumber > 0) {
+      intensity = 255;
+  }
+}
+
+// called when you want to send a message:
+function sendMqttMessage(shoulderpose) {
+ //console.log("hallo")
+  // if the client is connected to the MQTT broker:
+  if (client.isConnected()) {
+      // make a string with a random number form 0 to 15:
+     // let msg = String(round(random(15)));
+      let msg = JSON.stringify(shoulderpose);
+      // start an MQTT message:
+      message = new Paho.MQTT.Message(msg);
+     // console.log(message)
+      // choose the destination topic:
+      message.destinationName = topic;
+      // send it:
+      client.send(message);
+      // print what you sent:
+      localDiv.html('I sent: ' + message.payloadString);
+  }
+}
