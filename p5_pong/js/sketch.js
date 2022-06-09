7b7bb7bd8ed4b72abd7f01e7d8e3123564929f80
hfg-nyal-h <nyal.hettmer@hfg.design>
2022-06-09 21:42:07 +0200

    included handpose

--- p5_pong/js/sketch.js
+++ p5_pong/js/sketch.js
@@ -4,6 +4,10 @@ let video;
 let poseNet;
 let poses = [];
 let Average;
+let handpose;
+let predictions = [];
+let wave = undefined
+let waveCounter = 0
 //TODO lastavare without a funciton
 let lastAverage;
 
@@ -17,6 +21,7 @@ function setup() {
     p1 = new Paddle(width / 2 - 50, 20, height / 6, 10); //height / 6
     p2 = new Paddle(width / 2 - 50, height - 20, height / 6, 10);
     posenet5();
+    posenethand();
 
     // Create an MQTT client:
     client = new Paho.MQTT.Client(
@@ -42,6 +47,7 @@ function draw() {
     movePaddles();
     pop();
     backdrop();
+    drawKeypoints();
 
     p1.show();
     p2.show();
@@ -77,6 +83,7 @@ function movePaddles() {
     p2.move(width - Average - height / 6 / 2);
 }
 
+
 function keyTyped() {
     if (key == " ") {
         go = true;
@@ -93,18 +100,9 @@ function keyTyped() {
     return false;
 }
 
-// Copyright (c) 2018 ml5
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-/* ===
-ml5 Example
-PoseNet example using p5.js
-=== */
-
 function posenet5() {
     video = createCapture(VIDEO);
+    ////POSE DETECTION
 
     // Create a new poseNet method with a single detection
     poseNet = ml5.poseNet(video, modelReady);
@@ -126,7 +124,39 @@ function posenet5() {
     // Hide the video element, and just show the canvas
     video.hide();
 }
-
+ 
+function posenethand () {
+    //createCanvas(640, 480);
+    video2 = createCapture(VIDEO);
+    video2.size(width, height);
+  
+    handpose = ml5.handpose(video2, modelReady);
+  
+    // This sets up an event that fills the global variable "predictions"
+    // with an array every time new hand poses are detected
+    handpose.on("predict", results2 => {
+      predictions = results2;
+    });
+  
+    // Hide the video element, and just show the canvas
+    video2.hide();
+}
+ 
 function modelReady() {
     console.log("model loaded")
+    
+}
+
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  for (let i = 0; i < predictions.length; i += 1) {
+    const prediction = predictions[i];
+    for (let j = 0; j < prediction.landmarks.length; j += 1) {
+      const keypoint = prediction.landmarks[j];
+      fill(0, 255, 0);
+      noStroke();
+      ellipse(keypoint[0], keypoint[1], 10, 10);
+    }
+  }
 }
